3f5416da842282336ba7b51171d2841d1d881b66
sudheesh001 <sudheesh1995@outlook.com>
2014-05-01 18:24:43 +0300

    Basic Registration code, session

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'USERNAME';
+$db['default']['password'] = 'PASSWORD';
+$db['default']['database'] = 'Dispensary';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
---
+++ application/views/footer_view.php
@@ -0,0 +1,6 @@
+<div id="footer">
+  &copy; 2014 <a href="http://www.nitw.ac.in/">NITW</a> All Rights Reserved.
+ </div><!-- <div class="footer">-->
+ </div><!--<div id="wrapper">-->
+</body>
+</html>
